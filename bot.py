9fa8dee12dcd6db6cd1af3d4c74dcb558f4eb102
Vlad28slav <noconqueror@gmail.com>
2024-04-01 16:24:44 +0200

    first tried v

--- bot.py
+++ bot.py
@@ -1,34 +1,16 @@
-from telegram import Bot, Update, request
-from telegram.ext import Updater, CommandHandler
+import telebot
 from maim import convert_to_sound
-from pathlib import Path
-from urllib.request import Request
-from sqlalchemy import true
-from zmq import CONNECT_TIMEOUT
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 Token = os.getenv("Token")
 
+bot= telebot.TeleBot(Token)
 
-def changer_of_lang():
-    pass
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message, "Hey! Hows it going?")
 
-def main():
-    req= request(connect_timeout= 0.5)
-    my_bot= Bot(token= Token, request=req)
-    updater= Updater(bot= my_bot, use_context = True)
-    dp= Updater.dispatcher 
 
-#commands
-    cmd=[("change_language","change language from the default one")]
-    my_bot.set_my_commands(cmd)
-    dp.add_handler(CommandHandler(chandge_language, changer_of_lang))
-    updater.start_polling()
-    updater.idle()
-    
-
-if __name__=="__main__":
-    main()
-
+bot.polling
